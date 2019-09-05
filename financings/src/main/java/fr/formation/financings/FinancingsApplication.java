5e9f629e3c2b30d0a79cd330ac3b4d83db8c4a59
GeoffreyDu <geoffrey.ducci@gmail.com>
2019-09-05 17:35:09 +0200

    mapper (bug)

--- financings/src/main/java/fr/formation/financings/FinancingsApplication.java
+++ financings/src/main/java/fr/formation/financings/FinancingsApplication.java
@@ -1,13 +1,23 @@
 package fr.formation.financings;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration.AccessLevel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FinancingsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinancingsApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(FinancingsApplication.class, args);
+    }
 
+    @Bean
+    protected ModelMapper mapper() {
+	ModelMapper mapper = new ModelMapper();
+	mapper.getConfiguration().setFieldMatchingEnabled(true)
+		.setFieldAccessLevel(AccessLevel.PRIVATE);
+	return mapper;
+    }
 }
--- financings/src/main/java/fr/formation/financings/services/FinancingServiceImpl.java
+++ financings/src/main/java/fr/formation/financings/services/FinancingServiceImpl.java
@@ -2,11 +2,11 @@ package fr.formation.financings.services;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import fr.formation.financings.dtos.FinancingDto;
 import fr.formation.financings.dtos.FinancingViewDto;
-import fr.formation.financings.entities.Client;
 import fr.formation.financings.entities.Financing;
 import fr.formation.financings.repositories.ClientRepository;
 import fr.formation.financings.repositories.FinancingRepository;
@@ -18,17 +18,22 @@ public class FinancingServiceImpl implements FinancingService {
 
     private final ClientRepository clientRepo;
 
+    private final ModelMapper mapper;
+
     protected FinancingServiceImpl(FinancingRepository financingRepo,
-	    ClientRepository clientRepo) {
+	    ClientRepository clientRepo, ModelMapper mapper) {
 	this.financingRepo = financingRepo;
 	this.clientRepo = clientRepo;
+	this.mapper = mapper;
     }
 
     @Override
     public void create(FinancingDto dto) {
-	// Convert dto to entity:
-	Financing financing = new Financing();
-	populateAndSave(dto, financing);
+	// Convert DTO to entities
+	// Financing financing = new Financing();
+	// populateAndSave(dto, financing);
+	Financing entity = mapper.map(dto, Financing.class);
+	financingRepo.save(entity);
     }
 
     @Override
@@ -39,17 +44,21 @@ public class FinancingServiceImpl implements FinancingService {
     }
 
     private void populateAndSave(FinancingDto dto, Financing financing) {
-	financing.setName(dto.getName());
-	financing.setAmount(dto.getAmount());
-	financing.setReference(dto.getReference());
-	financing.setRate(dto.getRate());
-	financing.setStartDate(dto.getStartDate());
-	financing.setEndDate(dto.getEndDate());
-	financing.setValidated(dto.getValidated());
-	// Financing needs a Client object, not an id:
-	Client client = clientRepo.getOne(dto.getClientId());
-	financing.setClient(client);
-	financingRepo.save(financing);
+	// trandform DTO into Entity
+	Financing entity = mapper.map(dto, Financing.class);
+	financingRepo.save(entity);
+	// Remplacer par le mapping
+	/*
+	 * financing.setName(dto.getName());
+	 * financing.setAmount(dto.getAmount());
+	 * financing.setReference(dto.getReference());
+	 * financing.setRate(dto.getRate());
+	 * financing.setStartDate(dto.getStartDate());
+	 * financing.setEndDate(dto.getEndDate());
+	 * financing.setValidated(dto.getValidated()); // Financing needs a
+	 * Client object, not an id: Client client =
+	 * clientRepo.getOne(dto.getClientId()); financing.setClient(client);
+	 */
     }
 
     @Override
